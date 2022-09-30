12247c1188f03c2bb7bd8ede6b37e41abee76dfd
champ09D <darshanmhatre709@gmail.com>
2022-09-30 13:47:31 +0530

    Added : Employee Payroll Custom Exception Handlers Case  File Add

---
+++ src/main/java/com/example/springemployeepayrollapp/exception/CustomExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.springemployeepayrollapp.exception;
+
+import com.example.springemployeepayrollapp.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler
+{
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(CustomException exception)
+    {
+        ResponseDTO responseDTO = new ResponseDTO("Exception while parsing Rest request", exception.getMessage());
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+}
